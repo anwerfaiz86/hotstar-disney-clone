b897e3efd5a9a868d6dfcbc734391e741fcafc74
Md Faiz Anwer <faizanwerfaiz120@gmail.com>
2023-05-08 21:31:57 +0530

    hotstar-disney

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Login from "./components/Login";
+import Header from "./components/Header";
+import "./App.css";
+import Home from "./components/Home";
+import Detail from "./components/Detail";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Login />}></Route>
+          <Route exact path="/home" element={<Home />}></Route>
+          <Route exact path="/detail" element={<Detail />}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
